e4f483f92ffb2171a858e8e803eaa511fcdc800e
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-05-23 11:13:52 -0500

    ADD:index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html> 
+<head> 
+<title> Examen 2 </title> 
+ 
+</head> 
+Niebla es   una novela (nivola, según la denomina su autor) escrita por Miguel de Unamuno en 1907 y publicada en 1914 por la Editorial Renacimiento.
+</br>
+Narra la historia de Augusto Pérez y el problema existencial que vive al cuestionar su cotidianeidad y al visitar a Unamuno, quien le dice que es un ente de ficción. "  <a href="texto.php"> Ver texto </a>
+
+
+</body> 
+ 
+</html>
+
+
